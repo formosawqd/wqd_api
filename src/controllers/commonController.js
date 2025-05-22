83ebb3a5f654c45a2e3456199d9e935b28645d6e
formosawqd <formosadong@gmail.com>
2025-05-22 17:25:21 +0800

    获取产品问题

--- src/controllers/commonController.js
+++ src/controllers/commonController.js
@@ -2,21 +2,21 @@ const db = require("../models/db.js");
 
 // 获取分页数据
 const getLists = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query; // 默认每页10条数据，page为当前页
+  const { page = 1, pageSize = 10 } = req.query; // 默认每页10条数据，page为当前页
   try {
-    const offset = (page - 1) * limit;
+    const offset = (page - 1) * pageSize;
 
     // 查询数据库中的数据
     const [products] = await db.query(
       "SELECT * FROM products LIMIT ? OFFSET ?",
-      [Number(limit), Number(offset)]
+      [Number(pageSize), Number(offset)]
     );
 
     // 获取总数，计算总页数
     const [[{ total }]] = await db.query(
       "SELECT COUNT(*) AS total FROM products"
     );
-    const totalPages = Math.ceil(total / limit);
+    const totalPages = Math.ceil(total / pageSize);
 
     res.json({
       page: Number(page),
